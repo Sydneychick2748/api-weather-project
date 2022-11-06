f94eef3792f215f11b6bb7fcc92dae12eba02bf5
ana <ana@anniecannons.com>
2022-11-06 15:26:52 -0800

    add readme

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,3 @@
-// I WANT to see the weather outlook for multiple cities
-// SO THAT I can plan a trip accordingly
-// Acceptance Criteria
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city -->
-
-// <!-- hint____Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name? -->
-
 var lat;
 var lon;
 var apiKey = "7e4bd1e951cf0599deed23d6658120ae";
@@ -40,7 +25,6 @@ var cardBodyEl = document.querySelector(".card-body");
 var fiveDayEl = document.getElementById("fiveDayCard");
 
 var getCityWeather = function (lat, lon, city) {
-  debugger;
   const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
   fetch(weatherUrl).then(function (response) {
     if (response.ok) {
@@ -116,7 +100,7 @@ var getCityWeather = function (lat, lon, city) {
 
         // store in local storage
         var oldData = localStorage.getItem("city") || [];
-        debugger;
+
         var parsedOldData = oldData.length === 0 ? [] : JSON.parse(oldData);
 
         // if the thing that is already in there don't psuh
@@ -170,5 +154,7 @@ var formSubmitHandler = function (event) {
   var city = cityNameInputEl.value.trim();
   getCityLatLon(city);
 };
+//  how do i clear the local storage
+// localStorage.clear();
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
